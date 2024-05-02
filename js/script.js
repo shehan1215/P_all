665f5d602655962feb2dacbf2ef9abf8f027ae54
shehan1215 <kaushikashehan123@gmail.com>
2024-05-02 22:54:52 +0530

    created the promise

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
-const generateNo = new Promise((resolve, reject)=>{
-    const randomNo = Math.floor(Math.random()*10)
-    if(randomNo >=5){
-        resolve("the No is: " + randomNo)
-    }else{
-        reject("the No is less than 5")
-    }
-})
+// const generateNo = new Promise((resolve, reject)=>{
+//     const randomNo = Math.floor(Math.random()*10)
+//     if(randomNo >=5){
+//         resolve("the No is: " + randomNo)
+//     }else{
+//         reject("the No is less than 5")
+//     }
+// })
 
-generateNo.then(result => {
-    console.log(result);
-}).catch(error =>{
-    console.log(error);
-})
+// generateNo.then(result => {
+//     console.log(result);
+// }).catch(error =>{
+//     console.log(error);
+// })
+
+function getData(key, callback) {
+    return new Promise((resolve, reject) => {
+        console.log("getting data: ");
+        setTimeout(()=>{
+            if(key === "abc123"){
+                const data = "Raw data";
+                callback(null,data)
+            }else{
+                callback("Invalid key", null)
+            }
+
+        }, 1000);
+      
+    })
+    
+    
+}

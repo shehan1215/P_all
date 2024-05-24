afcdbb3949b00ae6736b08333efdc4442736a65f
shehan1215 <kaushikashehan123@gmail.com>
2024-05-24 11:00:31 +0530

    created the close button to the form

--- crud/client/src/App.js
+++ crud/client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { MdClose } from "react-icons/md";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <button className="btn btn-add">Add </button>
         <div className="addContainer">
           <form>
+            <div className="close-btn"><MdClose /></div>
             <label htmlFor="name">Name: </label>
             <input type="text" id="name" name="name"/>
 

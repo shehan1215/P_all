08bb89c84513415e06b0246d66665100affcc5ee
shehan1215 <kaushikashehan123@gmail.com>
2024-05-24 11:26:26 +0530

    use the useState func to prompt the form

--- crud/client/src/App.js
+++ crud/client/src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import { MdClose } from "react-icons/md";
+import { useState } from 'react';
 
 function App() {
+  const [addSection,setaddSection] = useState(false)
 
   const handleSubmit = (e)=>{
     e.preventDefault()
@@ -10,22 +12,28 @@ function App() {
   return (
     <>
       <div className="container">
-        <button className="btn btn-add">Add </button>
-        <div className="addContainer">
-          <form onSubmit={handleSubmit}>
-            <div className="close-btn"><MdClose /></div>
-            <label htmlFor="name">Name: </label>
-            <input type="text" id="name" name="name"/>
+        <button className="btn btn-add" onClick={()=>setaddSection (true)}>Add </button>
+        
+        {
+          addSection && (
+            <div className="addContainer">
+            <form onSubmit={handleSubmit}>
+              <div className="close-btn"><MdClose /></div>
+              <label htmlFor="name">Name: </label>
+              <input type="text" id="name" name="name"/>
+  
+              <label htmlFor="email">E-mail: </label>
+              <input type="email" id="email" name="email"/>
+  
+              <label htmlFor="mobile">Mobile: </label>
+              <input type="number" id="mobile" name="mobile"/>
+  
+              <button className="btn">Submit</button>
+            </form>
+          </div>
+          )
+        }
 
-            <label htmlFor="email">E-mail: </label>
-            <input type="email" id="email" name="email"/>
-
-            <label htmlFor="mobile">Mobile: </label>
-            <input type="number" id="mobile" name="mobile"/>
-
-            <button className="btn">Submit</button>
-          </form>
-        </div>
       </div>
     </>
   );

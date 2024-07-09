d53ce34cfaa746c360f8484221c06886a8f82141
shehan1215 <kaushikashehan123@gmail.com>
2024-07-09 09:43:31 +0530

    create DB connection

--- mernCrud/server.js
+++ mernCrud/server.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const port = 8000;
 const DB_URL = 'mongodb+srv://shehan:shehan123@mernapp.symu0zh.mongodb.net/crudMern?retryWrites=true&w=majority&appName=mernApp';
 
+mongoose.connect(DB_URL).then(()=>{
+    console.log('DB connect successfully');
+}).catch((err)=> console.log('DB connect unsuccessful', err))
 
 app.listen(port, ()=>{
     console.log(`App is already running on ${port}`);

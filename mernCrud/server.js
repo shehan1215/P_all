5d17e7a1531c411e7e1ad357efe84c1de347bfb6
shehan1215 <kaushikashehan123@gmail.com>
2024-07-09 09:38:34 +0530

    create DB

--- mernCrud/server.js
+++ mernCrud/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const port = 8000;
+const DB_URL = 'mongodb+srv://shehan:shehan123@mernapp.symu0zh.mongodb.net/crudMern?retryWrites=true&w=majority&appName=mernApp';
+
 
 app.listen(port, ()=>{
     console.log(`App is already running on ${port}`);

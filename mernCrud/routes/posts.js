4c3bc2a296033782ce7a24ba879c3ecd64d119d1
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-07-10 12:28:35 +0530

    Posts router created

---
+++ mernCrud/routes/posts.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const Posts = require('../models/posts');
+
+const router = express.Router();
+
+// To save the posts
+router.post('/post/save',(req,res)=>{
+    let newPost = new Posts(req.body);
+
+    newPost.save((err)=>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        }
+        return res.status(200).json({
+            success:'Posts save successfully Thank You!'
+        })
+    })
+})

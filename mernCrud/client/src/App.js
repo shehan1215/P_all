a9f439660ab26f1753abf02a3be981d1f38ca1f7
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-07-13 10:06:49 +0530

    commented

--- mernCrud/client/src/App.js
+++ mernCrud/client/src/App.js
@@ -21,7 +21,7 @@ export default class App extends Component {
         this.setState({
           posts:res.data.existingPosts
         })
-        console.log(this.state.posts); 
+        // console.log(this.state.posts); 
       }
     })
   }

95d388651482994fcb2bb3e90e618f7ead2c8e85
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-07-23 10:29:21 +0530

    corrected

--- mernCrud/client/src/App.js
+++ mernCrud/client/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 import{BrowserRouter,Route} from 'react-router-dom'
-import Home from './components/Home'
-import NavBar from './components/NavBar'
-import CreatePost from './components/CreatePost'
-import EditPost from './components/EditPost'
-import PostDetails from './components/PostDetails'
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import CreatePost from './components/CreatePost';
+import EditPost from './components/EditPost';
+import PostDetails from './components/PostDetails';
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <div className='container'>
           <NavBar/>
-          <Route path = "/" exact component={Home}></Route>
+          <Route exact path = "/" element ={Home}></Route>
           <Route path = "/add" component={CreatePost}></Route>
           <Route path = "/edit/:id" component={EditPost}></Route>
           <Route path = "/post/:id" component={PostDetails}></Route>

5651b741af89284d39d091e311ea22347aae3450
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-10-23 10:50:59 +0530

    learning the python basics

--- pyhton/first.py
+++ pyhton/first.py
@@ -1,1 +1,18 @@
-print('Hello World')
+x = [12,23,43,12]
+x.append(100)
+print(x)
+x.insert(3,500)
+print(x)
+y = [21,0,90,45]
+z = x + y
+x.pop(2)
+print(z)
+# is_in_the_list = 90 in z
+
+if(120 in z):
+    print("this value is in the list")
+else:
+    print("this value is not included")
+
+
+

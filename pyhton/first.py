20cf7a15c11d9f8bfbb5e80b5dc7fb2227d9c7e2
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-10-24 10:40:01 +0530

    learning the python basics

--- pyhton/first.py
+++ pyhton/first.py
@@ -1,21 +1,38 @@
-x = [12,23,43,12]
-x.append(100)
+# x = [12,23,43,12]
+# x.append(100)
+# print(x)
+# x.insert(3,500)
+# print(x)
+# y = [21,0,90,45]
+# z = x + y
+# x.pop(2)
+# print(z)
+# z.remove(23)
+# print("### After Remove the 23: ", z)
+# print(z)
+# # is_in_the_list = 90 in z
+
+# if(120 in z):
+#     print("this value is in the list")
+# else:
+#     print("this value is not included")
+
+
+
+
+x = {
+    'a':["hello","good morning"],
+    'b':["bye","good night"],
+    'c':29,
+}
+y = x['b']
+y.append("have a nice day")
 print(x)
-x.insert(3,500)
+
+z = x['c']
+z = 21
+print(z)
+_get = x.get("a")
+print(_get)
+del x['c']
 print(x)
-y = [21,0,90,45]
-z = x + y
-x.pop(2)
-print(z)
-z.remove(23)
-print("### After Remove the 23: ", z)
-print(z)
-# is_in_the_list = 90 in z
-
-if(120 in z):
-    print("this value is in the list")
-else:
-    print("this value is not included")
-
-
-

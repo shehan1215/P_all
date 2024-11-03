e48ee4e9e3a4ebd7d2e90ee5fc90b44b8d059d61
kaushikashehan123@gmail.com <kaushikashehan123@gmail.com>
2024-11-03 11:52:01 +0530

    created the database connection with mongodb

--- mernFullCrudPractice/backend/app.js
+++ mernFullCrudPractice/backend/app.js
@@ -1,1 +1,14 @@
-console.log("bye");
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express();
+//Connect the Middleware
+app.use("/",(req,res,next)=>{
+    res.send("Working no problem")
+});
+
+// connect the database
+mongoose.connect("mongodb+srv://admin:o7Pv1rWCjdK8k5qy@cluster0.iczfx.mongodb.net/")
+.then(()=>console.log("Database Connected")).then(()=>{
+    app.listen(5000);
+}).catch((err)=>console.log((err)));
